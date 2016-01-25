94b975dc01ea132887f1dc913a8cb3fe66a4b713
agnaldo <agnaldobernardojunior@yahoo.com.br>
2016-01-25 20:48:45 -0200

    Configuração do modulo

---
+++ module/Application/Module.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Application;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+          'factories' => array(
+              'Application\Service\Categoria' => function($sm) {
+                      $em = $sm->get('Doctrine\ORM\EntityManager');
+                      $categoriaService = new \Application\Service\Post($em);
+                      return $categoriaService;
+                  },
+              
+          )
+        );
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
